7cb5e4a489a9e31ac27b7d68056cfdbcdb11cba3
cesarTix <cesarsumano@icloud.com>
2017-07-18 18:07:18 -0500

    print en secondController desde la rma branchTest

--- Git/SecondViewController.swift
+++ Git/SecondViewController.swift
@@ -12,6 +12,7 @@ class SecondViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("Prueba branchCesar en secondViewController")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
